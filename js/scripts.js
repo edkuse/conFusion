80796457a8726b2c243bf13729eb2b14566c4f74
Eddie Kuse <edkuse@gmail.com>
2020-01-19 09:01:22 -0800

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
 $(document).ready(function(){
-    $("#carouselButton").click(function(){
-          if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-              $("#mycarousel").carousel('pause');
-              $("#carouselButton").children("span").removeClass('fa-pause');
-              $("#carouselButton").children("span").addClass('fa-play');
-          }
-          else if ($("#carouselButton").children("span").hasClass('fa-play')){
-              $("#mycarousel").carousel('cycle');
-              $("#carouselButton").children("span").removeClass('fa-play');
-              $("#carouselButton").children("span").addClass('fa-pause');
-          }
-      return;
-    });
+  $("#mycarousel").carousel( { interval: 2000 } );
 
-    $("#login-button").click(function(){
-      $("#loginmodal").modal('toggle');
-    });
-
-    $("#reservetbl-button").click(function(){
-      $("#reserve-modal").modal('toggle');
-    });
-
+  $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+          $("#mycarousel").carousel('pause');
+          $("#carouselButton").children("span").removeClass('fa-pause');
+          $("#carouselButton").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carouselButton").children("span").removeClass('fa-play');
+          $("#carouselButton").children("span").addClass('fa-pause');                    
+      }
   });
-  
+
+  // Open modal based on button clicked
+  $('.openmodal').click(function(e) {
+      e.preventDefault();
+
+      var id = this.id,
+           m = $('#' + id + '-modal');
+
+      // Toggles modal, should always open it
+      m.modal('toggle');
+  });
+});
